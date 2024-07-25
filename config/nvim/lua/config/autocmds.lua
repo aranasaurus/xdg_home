ef99f9522421c5dc0cd2f267e04e793028aed5f0
Ryan Arana <ryan.arana@hey.com>
2024-07-25 13:45:15 -0700

    Disable all overrides of formatoptions
    At first I just wanted to remove the "o" option from it which makes
    pressing "o" in Normal mode on a commented line start the new line as a
    commented line, which is almost never what I want. But after thinking
    about it, I really don't want the options that are available in
    formatoptions to change per filetype... I want them to always be the
    same. This is done with an autocommand on BufEnter.

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("No formatoptions overrides, please", { clear = true }),
+  callback = function()
+    vim.opt.formatoptions = "tcrqanblj"
+  end,
+})
