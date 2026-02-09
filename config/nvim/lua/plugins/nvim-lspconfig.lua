07db932c9fdce2452f0f14e08c4feee606cdb008
Ryan Arana <ryan.arana@hey.com>
2026-02-09 08:54:49 -0800

    add godot support

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,19 @@
+local cmd
+if vim.fn.has("win32") == 1 then
+  cmd = { "ncat", "localhost", "6005" }
+else
+  cmd = vim.lsp.rpc.connect("127.0.0.1", 6005)
+end
 return {
   {
     "nvim-lspconfig",
     opts = {
-      inlay_hints = { enabled = false },
+      servers = {
+        gdscript = {
+          name = "godot",
+          cmd = cmd,
+        },
+      },
     },
   },
 }
